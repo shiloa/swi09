d1d7305e219213276017cb6f53f9ff348b117e41
murkin <murkin@web109.webfaction.com>
2009-12-20 07:39:24 -0600

    added 404 html

--- theapp/admin.py
+++ theapp/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from swi09.theapp.models import *
 
 admin.site.register(UserInfo)
+admin.site.register(ContactRequest)
 admin.site.register(Company)
 admin.site.register(Plan)
 admin.site.register(Step)
